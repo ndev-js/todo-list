8edca12ae1bab12d7bb0bace011ff846745874e5
Nomi <81682052+ndev-js@users.noreply.github.com>
2024-03-08 17:47:17 +0500

    Update Listitems.jsx

--- src/Listitems.jsx
+++ src/Listitems.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
-const Listitems=(props)=>{
+const Listitems=({text)=>{
 const [line,setLine]=useState(false);
 const cutIt =() =>{
     setLine(true)
@@ -12,7 +12,7 @@ return(<>
           <div className='todostyle'>
               <span  onClick={cutIt}>
                   <DeleteForeverIcon  className="dell"/></span>
-          <li style={ {textDecoration: line ?'line-through':'none' }}>{props.text}</li>
+          <li style={ {textDecoration: line ?'line-through':'none' }}>{text}</li>
           </div>
 
 </>);
